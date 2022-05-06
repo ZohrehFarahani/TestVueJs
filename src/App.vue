d282be2196d73893d00e1b3bc0e347cce95c7b86
ZohrehFarahani <71258395+ZohrehFarahani@users.noreply.github.com>
2022-05-06 11:42:52 +0430

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,18 @@
       <div class="row">
         <div class="col-6">
           <card>
-              <form class="form-inline">
+            
   
   <div class="form-group mx-sm-3 mb-2">
       <input  type="text" class="form-control" v-model="mess" placeholder="لطفا تایپ نمایید" />
   </div>
   <button v-on:click="sendMessage()" class="btn btn-primary">ارسال</button>
-</form>
-   
-   
-    <div>{{showMessage}}</div>
+
+   <br>
+    <span v-show="visibale">ارتباط با موفقیت انجام شد</span>
+    <br>
+     
+          <span v-text="result"></span>
      
           </card>
 </div>
@@ -38,44 +40,49 @@ export default {
   data: function() {
     return {
       connection: null,
-      showMessage:''
+      show:null,
+      visibale:false,
+      result:null
     }
   },
   methods: {
-    sendMessage: function() {
-      console.log(this.connection);
-      this.connection.send(this.mess);
-    }
-  },
-  created: function() {
-
-    // this.showMessage = "شروع اتصال به وب سوکت"
-    this.connection = new WebSocket("wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
-
-    this.connection.onmessage = function(event) {
-      console.log("message:"+event.data);
-       this.showMessage=event.data;
+    sendMessage: function() {  
+      this.connection.send(this.mess)  
+    },
+     connect : function() {
+     this.result = "نتیجه..";
+     this.result = "وضعیت ارتباط..";
+     this.connection = new WebSocket("wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
+     this.connection.onmessage =(message)=> {
+       console.log(message.data)
+    this.result = message.data
+    
     }
 
-    this.connection.onopen = function(event) { 
-     this.showMessage= "ارتباط با موفقیت انجام شد"
+    this.connection.onopen = function(event) {    
       console.log(event)
-      console.log ("ارتباط با موفقیت انجام شد")
+      console.log ("ارتباط با موفقیت انجام شد"+event.data)
+     this.show ="ارتباط با موفقیت انجام شد"
+      this.visibale =true
      
     }
     this.connection.onerror = function(err) {
-   this.showMessage = 'خطا در سوکت ', err.message, 'سوکت بسته شده است';
-    this.connection.close();
-    this.connection =new WebSocket("wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
-    this.connection.reconnectInterval = 60000
+      this.show = 'خطا در سوکت ', err.message, 'سوکت بسته شده است'
+      this.connection.close()
      }
-    //  this.connection.onmessage = function (message) {
-    //  const data = JSON.parse(message.data);
-    //  console.log("message data"+data);
-    //  }
-  
-
+    this.connection.onclose = function(err) {
+        console.log('سوکت بسته شده است . ارتباط مجدد'+err.message)
+         this.show ='سوکت بسته شده است . ارتباط مجدد'+err.message
+       setTimeout(function() {
+       this.connect()
+        }, 1000)
+     }
   }
+  },
+  created: function() {
+      this.connect()
+  },
+  
 }
 </script>
 
